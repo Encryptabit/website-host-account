6bae9019d0aed9b898eb5c83998c4822e0964eca
Encryptabit <jacari.jones@live.com>
2022-08-02 14:06:44 -0700

    Module federationw orking in Host

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
+import { LoginActivityComponent } from './components/login-activity/login-activity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PersonalInfoComponent,
+    LoginActivityComponent
   ],
   imports: [
     BrowserModule,
